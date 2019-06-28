dc226ef51274dea17db2d3028c21c6510928e574
p0x0q <p0x0q_jp@gmail.com>
2019-06-28 23:13:58 +0900

    u

--- popup.js
+++
@@ -1,9 +0,0 @@
-	jQuery(document).ready(function(){
-		chrome.storage.local.get(["runnerloop_base"], function (value) {
-		jQuery("#textbox_1").val(value.runnerloop_base);
-		});
-	});
-    jQuery('#save_button').click(function(){
-		chrome.storage.local.set({'runnerloop_base': jQuery("#textbox_1").val()}, function () {});
-        document.write("Option saved.");
-    });
