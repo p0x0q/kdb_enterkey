551893e8ba25777efd72f9ba82ad8c88446ac44a
p0x0q <p0x0q_jp@gmail.com>
2019-09-27 14:12:53 +0900

    support new version

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,9 @@
-
 $(document).ready(function(){
+	jQuery('#freeWord').keypress( function ( e ) {
+		if ( e.which == 13 ) {
+			jQuery("#openingCourseListForm table:first-of-type td input:first-child").click();
+		}
+	});
 	jQuery('#txtSyllabus').keypress( function ( e ) {
 		if ( e.which == 13 ) {
 			jQuery("#btnSearch").click();
