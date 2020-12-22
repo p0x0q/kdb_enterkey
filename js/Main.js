3ee8afb1bab4e5329a3e7595b715c62d2eafd3fd
p0x0q <p0x0q_jp@gmail.com>
2020-12-22 16:31:28 +0900

    support new version

--- js/Main.js
+++ js/Main.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
+	// jQuery('#freeWord').keypress( function ( e ) {
+	// 	if ( e.which == 13 ) {
+	// 		jQuery("#openingCourseListForm table:first-of-type td input:first-child").click();
+	// 	}
+	// });
+	// jQuery('#txtSyllabus').keypress( function ( e ) {
+	// 	if ( e.which == 13 ) {
+	// 		jQuery("#btnSearch").click();
+	// 	}
+	// });
 	jQuery('#freeWord').keypress( function ( e ) {
 		if ( e.which == 13 ) {
-			jQuery("#openingCourseListForm table:first-of-type td input:first-child").click();
+			jQuery("#openingCourseListForm table td input[type=button]:nth-child(2)").click();
 		}
 	});
-	jQuery('#txtSyllabus').keypress( function ( e ) {
-		if ( e.which == 13 ) {
-			jQuery("#btnSearch").click();
-		}
-	});
+
 });
